7ef0f35f31a59c8dc01d210e5f586d33d7f19ff7
Pedro Albano <phmello8@gmail.com>
2023-11-21 11:39:56 -0300

    First prototype structure
    First wip structure test. I'm changing the design to a gates and locks mechanic, with each lock needing a secret word to be unlocked and each gate has its own theme defined in a separate JSON file structure

---
+++ SuperHangingManGame/SuperHangingManGame/GateDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class GateDrawer
+{
+    public static void DrawGate(int numberOfLocks)
+    {
+        Console.Clear();
+
+        Console.WriteLine("  +---+");
+        Console.WriteLine("  |   |");
+
+        for (int i = 0; i < numberOfLocks; i++)
+        {
+            Console.Write("  X   |");
+        }
+
+        Console.WriteLine("\n      |");
+        Console.WriteLine("=========");
+    }
+
+    public static void UpdateLockStatus(int lockIndex)
+    {
+        Console.SetCursorPosition(4 + lockIndex * 6, Console.CursorTop - 1);
+        Console.Write("O");
+    }
+}
